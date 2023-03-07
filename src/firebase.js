f1ed313ed2222c5f2cdf3bc8213c1b6ddce8f5df
matu <salinasmateo@oulook.com>
2023-03-07 18:52:40 -0300

    change favicon

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  projectId: VITE_PROJECT,
-  storageBucket:VITE_STORAGE,
-  messagingSenderId: VITE_MESSAGIN,
-  appId: VITE_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGIN,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
